72193fed683420fc8b30aad499e9e0ed6b81574c
dilmithp <dilmithp@outlook.com>
2024-11-17 18:47:53 +0530

    2nd commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+</main>
+<footer>
+  <div class="container">
+    <p>&copy; <?php echo date('Y'); ?> ToDoWithMe. All Rights Reserved.</p>
+    <ul>
+      <li><a href="privacy.php">Privacy Policy</a></li>
+      <li><a href="terms.php">Terms of Service</a></li>
+    </ul>
+  </div>
+</footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+
+<section>
+  <h2>Welcome to ToDoWithMe</h2>
+  <p>Organize your tasks and achieve more!</p>
+</section>
+
+<?php include 'footer.php'; ?>
